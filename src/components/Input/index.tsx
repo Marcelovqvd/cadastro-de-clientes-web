7ead399f9825c6adcf66405aa125d83c5ffcfd81
Marcelo Vasques <marcelovqvd@gmail.com>
2020-08-11 13:54:14 -0300

    form

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,33 @@
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  email: string;
+  birth: string;
+  cpf: number;
+  whatsapp: number;
+  address: string;
+  obs: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+    </Container>
+  );
+};
+export default Input;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { Title } from './styles';
+
+import Input from '../../components/Input';
 
 const Register: React.FC = () => {
-  return <h1>Register</h1>
-}
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido'),
+        birth: Yup.string().required('Data de nascimento obrigatória'),
+        cpf: Yup.number().required('CPF obrigatório'),
+        whatsapp: Yup.number().required('whatsapp obrigatório'),
+        address: Yup.string().required('Endereço obrigatório'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <>
+      <Title>Faça seu cadastro</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Digite seu nome" />
+        <Input name="email" placeholder="Digite seu email" />
+        <Input
+          type="text"
+          name="birth"
+          placeholder="Digite sua data de nascimento"
+        />
+        <Input name="cpf" placeholder="Digite seu cpf" />
+        <Input name="whatsapp" placeholder="Digite seu whatsapp" />
+        <Input name="address" placeholder="Digite seu endereço" />
+        <textarea name="obs" placeholder="Deixe sua mensagem" />
+        <button type="submit">Cadastrar</button>
+      </Form>
+    </>
+  );
+};
 
 export default Register;
