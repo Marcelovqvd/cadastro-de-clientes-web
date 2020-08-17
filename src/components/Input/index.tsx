4ae5c9135a7b0d4c495eef9ceb61ab12f69a530a
Marcelo Vasques <marcelovqvd@gmail.com>
2020-08-17 11:32:51 -0300

    button e input component - spread rule

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,21 @@
 import React, { InputHTMLAttributes } from 'react';
 
-import './styles';
+import { Container } from './styles';
+
+// este componente input recebe todas as propriedades q um input html tradicional receberia -> InputHTMLAttributes
+// O InputHTMLAttributes recebe um parâmetro de tipagem HTMLInputElement que está global na aplicação
+// Aqui vai sobrescrever o name, tornando-o obrigatório
+// desabilitar a regra em eslintrc.json: Prop spreading is forbiddeneslintreact/jsx-props-no-spreading
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  label: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+const Input: React.FC<InputProps> = props => {
   return (
-    <div className="input-block">
-      <label htmlFor={name}>{label}</label>
-      <input type="text" id={name} {...rest} />
-    </div>
+    <Container>
+      <input {...props} />
+    </Container>
   );
 };
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
-import { Container, Content, Background } from './styles';
-
 import logoImg from '../../assets/logoImg.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
   return (
@@ -13,9 +16,9 @@ const SignUp: React.FC = () => {
         <img src={logoImg} alt="logo" />
         <form>
           <h1>Entre com seu login e senha</h1>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
+          <Input name="input" type="text" placeholder="Email" />
+          <Input name="input" type="password" placeholder="Senha" />
+          <Button type="submit">Entrar</Button>
           <a href="/forgot">Esqueceu sua senha?</a>
         </form>
         <Link to="/register">
