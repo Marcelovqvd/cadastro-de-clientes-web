f74b41f4d00486091d198a093df4fe924577688f
Marcelo Vasques <marcelovqvd@gmail.com>
2020-08-17 11:41:14 -0300

    component button - children

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,10 +8,10 @@ import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = () => {
+const Button: React.FC<ButtonProps> = ({children, ...rest}) => {
   return (
     <Container>
-      <button type="button">clique</button>
+      <button {...rest}>{children}</button>
     </Container>
   );
 };
